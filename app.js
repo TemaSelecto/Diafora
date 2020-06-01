b0c2c75f85ddaf4a27c6446b2345c4f2a9b75eee
Manuel Figueroa <mfigueroacr@gmail.com>
2020-05-31 18:02:43 -0600

    test

--- app.js
+++ app.js
@@ -60,20 +60,20 @@ module.exports = app;
 
 
 
-// //cross domain proxy
-// // Listen on a specific host via the HOST environment variable
-// var host = process.env.HOST || '127.0.0.1';
-// // Listen on a specific port via the PORT environment variable
-// var port = process.env.PORT || 5757;
+//cross domain proxy
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '127.0.0.1';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 5757;
  
-// var cors_proxy = require('cors-anywhere');
-// cors_proxy.createServer({
-//     originWhitelist: [], // Allow all origins
-//     requireHeader: ['origin', 'x-requested-with'],
-//     removeHeaders: ['cookie', 'cookie2']
-// }).listen(port, host, function() {
-//     console.log('Running CORS Anywhere on ' + host + ':' + port);
-// });
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
 
 
 
